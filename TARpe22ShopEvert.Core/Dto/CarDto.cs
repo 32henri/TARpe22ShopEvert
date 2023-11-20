1426f956f6c2e0c0a68856b7e72d892108c8b5c0
Opilane TTHK <opilane@tthk.ee>
2023-11-20 11:37:20 +0200

    yes car
    car yes

---
+++ TARpe22ShopEvert.Core/Dto/CarDto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARpe22ShopEvert.Core.Dto
+{
+    public enum CarType
+    {
+        BMW, Audi, Honda, Toyota, Bently, Subaru, Jeep, Lamborghini, Ford
+    }
+
+    public class CarDto
+    {
+        public Guid Id { get; set; }
+        public string Mark { get; set; }
+        public int Price { get; set; }
+        public string Model { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsNew { get; set; }
+        public int HorsePower { get; set; }
+
+    }
+}
