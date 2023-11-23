56a2ff09946a5fe5a696e20e06191a49dce9e812
henri <127192852+32henri@users.noreply.github.com>
2023-11-23 12:43:04 +0200

    Add files via upload

---
+++ CarDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TARpe22ShopEvert.Models.RealEstate;
+
+namespace TARpe22ShopEvert.Models.Cars
+{
+    public class CarDetailsViewModel : Controller
+    {
+        public Guid Id { get; set; }
+        public string Mark { get; set; }
+        public int Price { get; set; }
+        public string Model { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsNew { get; set; }
+        public int HorsePower { get; set; }
+        public List<FileToApiViewModelCar> FileToApiViewModelCar { get; set; } = new List<FileToApiViewModelCar>();
+    }
+}
