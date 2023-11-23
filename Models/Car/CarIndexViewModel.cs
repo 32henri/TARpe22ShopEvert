5117b19d16d6d92d54c0ba49719badc22a4965cc
henri <127192852+32henri@users.noreply.github.com>
2023-11-23 12:42:01 +0200

    Add files via upload

---
+++ Models/Car/CarIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TARpe22ShopEvert.Models.Cars
+{
+    public class CarIndexViewModel : Controller
+    {
+        public Guid Id { get; set; }
+        public string Mark { get; set; }
+        public int Price { get; set; }
+        public string Model { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsNew { get; set; }
+        public int HorsePower { get; set; }
+    }
+}
